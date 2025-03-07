82c602311e548d69c48746339c08ce14427691cb
Matheus Felipe Pimenta <mfpimenta@minha.fag.edu.br>
2025-03-07 12:33:40 -0300

    Adicionando Hello.java na pasta Aula01

---
+++ Alunos/Matheus_Felipe_Pimenta/Aula01/Hello.java
@@ -0,0 +1,17 @@
+package aula;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        olaMundo();
+        holaQueTal();
+    }
+
+    public static void olaMundo() {
+        System.out.println("Olá Mundo");
+    }
+
+    public static void holaQueTal() {
+        System.out.println("Hola que tal");
+    }
+}
