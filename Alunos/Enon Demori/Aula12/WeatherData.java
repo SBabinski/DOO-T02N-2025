278e52371a52bb19db4c2ab0ce431a2c30155de1
AKAEnon <enondemori@gmail.com>
2025-06-17 18:01:42 -0300

    Commit aula 12 visual crossing

---
+++ Alunos/Enon Demori/Aula12/WeatherData.java
@@ -0,0 +1,63 @@
+    package enon;
+
+public class WeatherData {
+
+    private double temperaturaAtual;
+    private double temperaturaMaxima;
+    private double temperaturaMinima;
+    private double umidade;
+    private double precipitacao;
+    private String condicao;
+    private double ventoVelocidade;
+    private String ventoDirecao;
+    
+    public double getTemperaturaAtual() {
+        return temperaturaAtual;
+    }
+    public void setTemperaturaAtual(double temperaturaAtual) {
+        this.temperaturaAtual = temperaturaAtual;
+    }
+    public double getTemperaturaMaxima() {
+        return temperaturaMaxima;
+    }
+    public void setTemperaturaMaxima(double temperaturaMaxima) {
+        this.temperaturaMaxima = temperaturaMaxima;
+    }
+    public double getTemperaturaMinima() {
+        return temperaturaMinima;
+    }
+    public void setTemperaturaMinima(double temperaturaMinima) {
+        this.temperaturaMinima = temperaturaMinima;
+    }
+    public double getUmidade() {
+        return umidade;
+    }
+    public void setUmidade(double umidade) {
+        this.umidade = umidade;
+    }
+    public double getPrecipitacao() {
+        return precipitacao;
+    }
+    public void setPrecipitacao(double precipitacao) {
+        this.precipitacao = precipitacao;
+    }
+    public String getCondicao() {
+        return condicao;
+    }
+    public void setCondicao(String condicao) {
+        this.condicao = condicao;
+    }
+    public double getVentoVelocidade() {
+        return ventoVelocidade;
+    }
+    public void setVentoVelocidade(double ventoVelocidade) {
+        this.ventoVelocidade = ventoVelocidade;
+    }
+    public String getVentoDirecao() {
+        return ventoDirecao;
+    }
+    public void setVentoDirecao(String ventoDirecao) {
+    this.ventoDirecao = ventoDirecao;
+}
+    
+}
